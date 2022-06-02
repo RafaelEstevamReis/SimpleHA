6642ae68411892a3900f9d19c596a9e591417b44
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-06-02 19:58:53 -0300

    Sample

---
+++ SimpleHA.Tests/Program.cs
@@ -0,0 +1,10 @@
+﻿// Put yout token in a text file
+
+using Simple.HAApi;
+
+string haToken = File.ReadAllLines("token.txt").First();
+
+var instance = new Instance(new Uri("http://127.0.0.1:8213"), haToken);
+
+
+Console.WriteLine($"IsOnline: { await instance.CheckRunningAsync() }" );
