994dd791548d993e7b54a70f7d94d9c2f171addf
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-06-02 22:07:31 -0300

    Create Camera.cs

---
+++ Simple.HAApi/Sources/Camera.cs
@@ -0,0 +1,22 @@
+﻿using Simple.API;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Simple.HAApi.Sources
+{
+    public class Camera : SourceBase
+    {
+        public Camera(ClientInfo info)
+           : base(info)
+        { }
+
+        public async Task<byte[]> GetImageAsync(string cameraEntityId)
+            => await GetAsync<byte[]>($"/api/camera_proxy/{cameraEntityId}");
+        public async Task SaveCameraImageAsync(string cameraEntityId, string path)
+        {
+            var bytes = await GetAsync<byte[]>($"/api/camera_proxy/{cameraEntityId}");
+            File.WriteAllBytes(path, bytes);
+        }
+
+    }
+}
