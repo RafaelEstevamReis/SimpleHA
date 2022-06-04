e643df8c0e0f24719fbb6145f7e0ea98bf668b91
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-06-04 14:32:25 -0300

    Add button

--- Simple.HAMQTT/Models/DeviceRegistry.cs
+++ Simple.HAMQTT/Models/DeviceRegistry.cs
@@ -89,4 +89,28 @@ namespace Simple.HAMQTT.Models
         public string PayloadOff { get; set; }
     }
 
+    public class ButtonRegistry : DeviceRegistry
+    {
+        public ButtonRegistry()
+        {
+            Component = "button";
+        }
+
+        [JsonProperty("command_topic", NullValueHandling = NullValueHandling.Ignore)]
+        public string CommandTopic { get; set; }
+        [JsonProperty("payload_press ", NullValueHandling = NullValueHandling.Ignore)]
+        public string PayloadPress { get; set; }
+
+        public static ButtonRegistry Build(string displayName, string commandTopic, string payloadPress, string deviceId, string availabilityTopic = null)
+        {
+            return new ButtonRegistry
+            {
+                Name = displayName,
+                CommandTopic = commandTopic,
+                PayloadPress = payloadPress,
+                DeviceId = deviceId,
+                AvailabilityTopic = availabilityTopic,
+            };
+        }
+    }
 }
