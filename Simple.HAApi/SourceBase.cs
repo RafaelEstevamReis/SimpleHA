d130c4056f70b837fa5a651bda544b135eb28cd6
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-07-13 00:08:51 -0300

    Add PostJson

--- Simple.HAApi/SourceBase.cs
+++ Simple.HAApi/SourceBase.cs
@@ -1,4 +1,6 @@
 ﻿using Simple.API;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Simple.HAApi
@@ -21,6 +23,14 @@ namespace Simple.HAApi
         protected async Task<T> PostAsync<T>(string path, object data)
         {
             var info = await client.PostAsync<T>(path, data);
+            if (!info.IsSuccessStatusCode) throw new Exceptions.ClientExeption($"Request to `{path}` Failed with code {info.StatusCode}", info);
+
+            return info.Data;
+        }
+        protected async Task<T> PostJsonAsync<T>(string path, string json)
+        {
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var info = await client.PostAsync<T>(path, content);
             if (!info.IsSuccessStatusCode) throw new Exceptions.ClientExeption($"Request to `{path}` Failed with code {info.StatusCode}", info);
 
             return info.Data;
