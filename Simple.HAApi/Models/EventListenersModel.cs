306dcee8facd60a987a14d4576369a412a51cc79
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-07-12 22:53:35 -0300

    Create EventListenersModel.cs

---
+++ Simple.HAApi/Models/EventListenersModel.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace Simple.HAApi.Models
+{
+    public class EventListenersModel
+    {
+        public string Event { get; set; }
+        [JsonProperty("listener_count")]
+        public int ListenerCount { get; set; }
+    }
+}
