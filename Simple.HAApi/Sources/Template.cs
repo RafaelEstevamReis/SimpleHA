5d0f239a3f05f66c34fe676c9fac54ea8d62d23b
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-07-12 22:56:36 -0300

    Create Template.cs

---
+++ Simple.HAApi/Sources/Template.cs
@@ -0,0 +1,16 @@
+﻿using Simple.API;
+using System.Threading.Tasks;
+
+namespace Simple.HAApi.Sources
+{
+    public class Template: SourceBase
+    {
+        public Template(ClientInfo info)
+            : base(info)
+        { }
+
+        public async Task<string> RenderTemplateAsync(string template)
+            => await PostAsync<string>("/api/template", new { template });
+
+    }
+}
