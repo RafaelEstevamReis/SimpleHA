57e22b4735d8d1e3c6979b1f9ee2f0b40e328f70
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-08-19 01:12:03 -0300

    Create DeviceTrigger.cs

---
+++ Simple.HAMQTT/Models/DeviceTrigger.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+
+namespace Simple.HAMQTT.Models
+{
+    public class DeviceTrigger : DeviceRegistry
+    {
+        // https://www.home-assistant.io/integrations/device_trigger.mqtt/
+        public DeviceTrigger()
+        {
+            Component = "device_automation";
+            AutomationType = "trigger";
+        }
+
+        [JsonProperty("automation_type", NullValueHandling = NullValueHandling.Ignore)]
+        public string AutomationType { get; set; }
+
+        [JsonProperty("topic", NullValueHandling = NullValueHandling.Ignore)]
+        public string Topic { get; set; }
+
+        [JsonProperty("type", NullValueHandling = NullValueHandling.Ignore)]
+        public string Type { get; set; }
+        [JsonProperty("subtype", NullValueHandling = NullValueHandling.Ignore)]
+        public string SubType { get; set; }
+
+        public static DeviceTrigger Build(string topic, int buttonNumber, string serialNumber, string deviceId)
+        {
+            return new DeviceTrigger()
+            {
+                Topic = topic,
+                Type = "button_short_press",
+                SubType = $"button_{buttonNumber}",
+                Device = new DeviceInfo()
+                {
+                    Identifiers = new string[] { serialNumber },
+                },
+                DeviceId = deviceId,
+            };
+        }
+    }
+}
