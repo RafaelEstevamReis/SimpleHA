c035fde517f61fc4b2ea8d8c6511392d5dd58147
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-08-19 22:48:08 -0300

    Add Classes

--- Simple.HAMQTT/Models/BinarySensorRegistry.cs
+++ Simple.HAMQTT/Models/BinarySensorRegistry.cs
@@ -35,5 +35,36 @@ namespace Simple.HAMQTT.Models
                 AvailabilityTopic = availabilityTopic,
             };
         }
+
+        public const string CLASS_NONE = "None";
+        public const string CLASS_BATTERY = "battery";
+        public const string CLASS_BATTERY_CHARGING = "battery_charging";
+        public const string CLASS_CARBON_MONOXIDE = "carbon_monoxide";
+        public const string CLASS_COLD = "cold";
+        public const string CLASS_CONNECTIVITY = "connectivity";
+        public const string CLASS_DOOR = "door";
+        public const string CLASS_GARAGE_DOOR = "garage_door";
+        public const string CLASS_GAS = "gas";
+        public const string CLASS_HEAT = "heat";
+        public const string CLASS_LIGHT = "light";
+        public const string CLASS_LOCK = "lock";
+        public const string CLASS_MOISTURE = "moisture";
+        public const string CLASS_MOTION = "motion";
+        public const string CLASS_MOVING = "moving";
+        public const string CLASS_OCCUPANCY = "occupancy";
+        public const string CLASS_OPENING = "opening";
+        public const string CLASS_PLUG = "plug";
+        public const string CLASS_POWER = "power";
+        public const string CLASS_PRESENCE = "presence";
+        public const string CLASS_PROBLEM = "problem";
+        public const string CLASS_RUNNING = "running";
+        public const string CLASS_SAFETY = "safety";
+        public const string CLASS_SMOKE = "smoke";
+        public const string CLASS_SOUND = "sound";
+        public const string CLASS_TAMPER = "tamper";
+        public const string CLASS_UPDATE = "update";
+        public const string CLASS_VIBRATION = "vibration";
+        public const string CLASS_WINDOW = "window";
+
     }
 }
