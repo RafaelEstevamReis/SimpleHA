266ba4ce94fd7b6ed1f6a33f8a9da82bb9415872
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-08-19 22:48:02 -0300

    Add Classes

--- Simple.HAMQTT/Models/DeviceTrigger.cs
+++ Simple.HAMQTT/Models/DeviceTrigger.cs
@@ -36,5 +36,24 @@ namespace Simple.HAMQTT.Models
                 DeviceId = deviceId,
             };
         }
+
+        public const string TYPE_BUTTON_SHORT_PRESS = "button_short_press";
+        public const string TYPE_BUTTON_SHORT_RELEASE = "button_short_release";
+        public const string TYPE_BUTTON_LONG_PRESS = "button_long_press";
+        public const string TYPE_BUTTON_LONG_RELEASE = "button_long_release";
+        public const string TYPE_BUTTON_DOUBLE_PRESS = "button_double_press";
+        public const string TYPE_BUTTON_TRIPLE_PRESS = "button_triple_press";
+        public const string TYPE_BUTTON_QUADRUPLE_PRESS = "button_quadruple_press";
+        public const string TYPE_BUTTON_QUINTUPLE_PRESSS = "button_quintuple_press";
+
+        public const string SUBTYPE_TURN_ON = "turn_on";
+        public const string SUBTYPE_TURN_OFF = "turn_off";
+        public const string SUBTYPE_BUTTON_1 = "button_1";
+        public const string SUBTYPE_BUTTON_2 = "button_2";
+        public const string SUBTYPE_BUTTON_3 = "button_3";
+        public const string SUBTYPE_BUTTON_4 = "button_4";
+        public const string SUBTYPE_BUTTON_5 = "button_5";
+        public const string SUBTYPE_BUTTON_6 = "button_6";
+
     }
 }
