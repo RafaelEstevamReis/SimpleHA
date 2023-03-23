b781b7864d8cb13d1af3701e1983122391ef79dd
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-03-23 00:08:23 -0300

    Add History source

---
+++ Simple.HAApi/Models/HistoryModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Simple.HAApi.Models
+{
+    public class HistoryModel
+    {
+        public EntityStateChangeModel[][] Items { get; set; }
+    }
+    public class EntityStateChangeModel
+    {
+        public string entity_id { get; set; }
+        public object state { get; set; }
+        public Dictionary<string, string> attributes { get; set; }
+        public DateTime last_changed { get; set; }
+        public DateTime last_updated { get; set; }
+    }
+}
---
+++ Simple.HAApi/Sources/History.cs
@@ -0,0 +1,60 @@
+﻿using Simple.API;
+using Simple.HAApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace Simple.HAApi.Sources
+{
+    public class History : SourceBase
+    {
+        public History(ClientInfo info)
+           : base(info)
+        { }
+
+        /// <summary>
+        /// Returns an array of state changes in the past. Each object contains further details for the entities.
+        /// </summary>
+        /// <param name="startDate">It determines the beginning of the period.</param>
+        /// <param name="endDate">It determines the end of the period</param>
+        /// <param name="minimalResponse">Only return last_changed and state for states other than the first and last state (much faster)</param>
+        /// <param name="includeAttributes">Determines whenever return attributes from the database (much faster)</param>
+        /// <param name="significantChangesOnly">Returns significant state changes</param>
+        /// <param name="filterIds">Filter on one or more entities, use NULL to return all entities</param>
+        public async Task<HistoryModel> GetPeriodAsync(DateTime startDate, DateTime endDate,
+                                         bool minimalResponse = true, bool includeAttributes = false,
+                                         bool significantChangesOnly = true,
+                                         string[] filterIds = null)
+        {
+            if (filterIds != null && filterIds.Length == 0)
+            {
+                throw new ArgumentException("filterIds should be null or greater than zero");
+            }
+
+            string timestamp = startDate.ToString("yyyy-MM-ddTHH:mm:sszzz", CultureInfo.InvariantCulture);
+            string strEndDate = endDate.ToString("yyyy-MM-ddTHH:mm:sszzz", CultureInfo.InvariantCulture);
+
+            var parameters = new List<string>()
+            {
+                $"end_time={strEndDate}",
+            };
+            if (filterIds != null)
+            {
+                parameters.Add($"filter_entity_id={string.Join(",", filterIds)}");
+            }
+            if (significantChangesOnly) parameters.Add("minimal_response");
+            if (minimalResponse) parameters.Add("significant_changes_only");
+            if (!includeAttributes) parameters.Add("no_attributes");
+
+            string url = $"/api/history/period/{timestamp}?{string.Join("&", parameters)}";
+            var result = await GetAsync<EntityStateChangeModel[][]>(url);
+
+            return new HistoryModel()
+            {
+                Items = result
+            };
+        }
+
+    }
+}
