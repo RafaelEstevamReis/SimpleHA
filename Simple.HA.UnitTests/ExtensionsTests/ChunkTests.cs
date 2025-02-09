3d9b4a41ac880ca4f3f06c6eb094a385c941e61f
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-02-09 12:55:54 -0300

    Add Chunk

---
+++ Simple.HA.UnitTests/ExtensionsTests/ChunkTests.cs
@@ -0,0 +1,40 @@
+﻿namespace Simple.HA.UnitTests.ExtensionsTests;
+
+using Simple.HAApi.Extensions;
+using System.Linq;
+using Xunit;
+
+public class ChunkTests
+{
+    [Fact]
+    public void Chunk_ArraySmallerThanChunkSize_ReturnsWholeArray()
+    {
+        int[] input = { 1, 2, 3 };
+        var result = NetExtensions.Chunk(input, 5).ToList();
+
+        Assert.Single(result);
+        Assert.Equal(input, result[0]);
+    }
+
+    [Fact]
+    public void Chunk_ArrayExactlyChunkSize_ReturnsWholeArray()
+    {
+        int[] input = { 1, 2, 3 };
+        var result = NetExtensions.Chunk(input, 3).ToList();
+
+        Assert.Single(result);
+        Assert.Equal(input, result[0]);
+    }
+
+    [Fact]
+    public void Chunk_ArrayLargerThanChunkSize_ReturnsChunks()
+    {
+        int[] input = { 1, 2, 3, 4, 5, 6, 7 };
+        var result = NetExtensions.Chunk(input, 3).ToList();
+
+        Assert.Equal(3, result.Count);
+        Assert.Equal(new[] { 1, 2, 3 }, result[0]);
+        Assert.Equal(new[] { 4, 5, 6 }, result[1]);
+        Assert.Equal(new[] { 7 }, result[2]);
+    }
+}
---
+++ Simple.HAApi/Extensions/NetExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Simple.HAApi.Extensions;
+
+using System.Collections.Generic;
+using System.Linq;
+
+public static class NetExtensions
+{
+    public static IEnumerable<T[]> Chunk<T>(this T[] source, int count)
+    {
+        if (source.Length <= count)
+        {
+            yield return source;
+            yield break;
+        }
+
+        for (int i = 0; i < source.Length; i += count)
+        {
+            yield return source.Skip(i).Take(count).ToArray();
+        }
+    }
+}
