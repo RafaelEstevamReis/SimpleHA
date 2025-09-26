aa5c5d1741899cf3ef56fb5757bceef2dab280fe
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-09-26 12:12:40 -0300

    Refactor Extensions namespaces

--- Simple.HAApi/Extensions/EntitiesExtensions.cs
+++ Simple.HAApi/Extensions/EntitiesExtensions.cs
@@ -1,40 +1,39 @@
-﻿using System.Collections.Generic;
+﻿namespace Simple.HAApi;
+
+using System.Collections.Generic;
 using System.Linq;
 
-namespace Simple.HAApi
+public enum Domains
 {
-    public enum Domains
-    {
-        AUTOMATION,
-        BINARY_SENSOR,
-        BUTTON,
-        CALENDAR,
-        CAMERA,
-        DEVICE_TRACKER,
-        LIGHT,
-        MEDIA_PLAYER,
-        PERSON,
-        SCRIPT,
-        SENSOR,
-        SUN,
-        SWITCH,
-        UPDATE,
-        WEATHER,
-        ZONE,
-    }
+    AUTOMATION,
+    BINARY_SENSOR,
+    BUTTON,
+    CALENDAR,
+    CAMERA,
+    DEVICE_TRACKER,
+    LIGHT,
+    MEDIA_PLAYER,
+    PERSON,
+    SCRIPT,
+    SENSOR,
+    SUN,
+    SWITCH,
+    UPDATE,
+    WEATHER,
+    ZONE,
+}
 
-    public static class EntitiesExtensions
-    {
-        public static IEnumerable<Models.StateModel> OfDomain(this IEnumerable<Models.StateModel> source, Domains domain)
-            => OfDomain(source, domain.ToString().ToLower());
-        public static IEnumerable<Models.StateModel> OfDomain(this IEnumerable<Models.StateModel> source, string domain)
-            => source.Where(o => o.Domain == domain);
+public static class EntitiesExtensions
+{
+    public static IEnumerable<Models.StateModel> OfDomain(this IEnumerable<Models.StateModel> source, Domains domain)
+        => OfDomain(source, domain.ToString().ToLower());
+    public static IEnumerable<Models.StateModel> OfDomain(this IEnumerable<Models.StateModel> source, string domain)
+        => source.Where(o => o.Domain == domain);
 
-        public static IEnumerable<string> GetIds(this IEnumerable<Models.StateModel> source)
-            => source.Select(o => o.EntityId);
+    public static IEnumerable<string> GetIds(this IEnumerable<Models.StateModel> source)
+        => source.Select(o => o.EntityId);
 
-        public static IEnumerable<(string Key, Models.StateModel[])> GroupByDomain(this IEnumerable<Models.StateModel> source)
-            => source.GroupBy(o => o.Domain).Select(g => (g.Key, g.ToArray()));
+    public static IEnumerable<(string Key, Models.StateModel[])> GroupByDomain(this IEnumerable<Models.StateModel> source)
+        => source.GroupBy(o => o.Domain).Select(g => (g.Key, g.ToArray()));
 
-    }
 }
