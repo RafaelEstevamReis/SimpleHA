31cc5ff45b58572aefb0858c462cfb89827f2e67
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-09-26 12:16:37 -0300

    Refactor base namespaces

--- Simple.HAApi/Instance.cs
+++ Simple.HAApi/Instance.cs
@@ -1,101 +1,100 @@
-﻿using Simple.API;
+﻿namespace Simple.HAApi;
+
+using Simple.API;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace Simple.HAApi
+public class Instance
 {
-    public class Instance
+    // Docs:
+    // https://developers.home-assistant.io/docs/api/rest/
+    // https://github.com/home-assistant/core/blob/dev/homeassistant/components/api/__init__.py
+
+    public Uri Uri { get; private set; }
+    public string Token { get; private set; }
+
+    public TimeSpan Timeout { get => client.Timeout; set => client.Timeout = value; }
+
+    public bool IgnoreCertificatErrors { get; set; } = false;
+
+    internal readonly ClientInfo client;
+
+    public Instance(Uri uri, string token)
+        : this(uri, token, null)
+    { }
+    public Instance(Uri uri, string token, HttpMessageHandler handler)
     {
-        // Docs:
-        // https://developers.home-assistant.io/docs/api/rest/
-        // https://github.com/home-assistant/core/blob/dev/homeassistant/components/api/__init__.py
+        Uri = uri;
+        Token = token;
 
-        public Uri Uri { get; private set; }
-        public string Token { get; private set; }
-
-        public TimeSpan Timeout { get => client.Timeout; set => client.Timeout = value; }
-
-        public bool IgnoreCertificatErrors { get; set; } = false;
-
-        internal readonly ClientInfo client;
-
-        public Instance(Uri uri, string token)
-            : this(uri, token, null)
-        { }
-        public Instance(Uri uri, string token, HttpMessageHandler handler)
-        {
-            Uri = uri;
-            Token = token;
-
-            if (handler == null) handler = new HttpClientHandler();
-            if (handler is HttpClientHandler hhdl) hhdl.ServerCertificateCustomValidationCallback ??= certificateValidation;
+        if (handler == null) handler = new HttpClientHandler();
+        if (handler is HttpClientHandler hhdl) hhdl.ServerCertificateCustomValidationCallback ??= certificateValidation;
 #if !NETSTANDARD
-            if (handler is SocketsHttpHandler shdl)
+        if (handler is SocketsHttpHandler shdl)
+        {
+            if(shdl.SslOptions != null) shdl.SslOptions = new System.Net.Security.SslClientAuthenticationOptions
             {
-                if(shdl.SslOptions != null) shdl.SslOptions = new System.Net.Security.SslClientAuthenticationOptions
-                {
-                    RemoteCertificateValidationCallback = certificateValidation,
-                };
-            }
+                RemoteCertificateValidationCallback = certificateValidation,
+            };
+        }
 #endif
 
-            client = new ClientInfo(Uri.ToString(), handler)
-            {
-                Timeout = TimeSpan.FromSeconds(10)
-            };
-            client.SetAuthorizationBearer(Token);
-        }
-        private bool certificateValidation(object sender,
-                                           System.Security.Cryptography.X509Certificates.X509Certificate certificate,
-                                           System.Security.Cryptography.X509Certificates.X509Chain chain,
-                                           System.Net.Security.SslPolicyErrors policy)
+        client = new ClientInfo(Uri.ToString(), handler)
         {
-            if (IgnoreCertificatErrors) return true;
-
-            return policy == System.Net.Security.SslPolicyErrors.None;
-        }
-        public void ConfigureInternalClient(Action<ClientInfo> action)
-        {
-            action(client);
-        }
-
-        public async Task<bool> CheckRunningAsync()
-        {
-            try
-            {
-                await IsRunningAsync();
-                return true;
-            }
-            catch { return false; }
-        }
-
-        public async Task IsRunningAsync()
-        {
-            var info = await client.GetAsync<string>("/api/");
-            if (!info.IsSuccessStatusCode) throw new Exceptions.ClientException($"Request to `/api/` Failed with code {info.StatusCode}", info);
-        }
-        public async Task<Models.CoreStateModel> GetCoreState()
-        {
-            var info = await client.GetAsync<Models.CoreStateModel>("/api/core/state");
-            if (!info.IsSuccessStatusCode) throw new Exceptions.ClientException($"Request to `/api/core/state` Failed with code {info.StatusCode}", info);
-
-            return info.Data;
-        }
-        public async Task<string> ErrorLogsAsync()
-        {
-            var info = await client.GetAsync<string>("/api/error_log");
-            if (!info.IsSuccessStatusCode) throw new Exceptions.ClientException($"Request to `/api/error_log` Failed with code {info.StatusCode}", info);
-
-            return info.Data;
-        }
+            Timeout = TimeSpan.FromSeconds(10)
+        };
+        client.SetAuthorizationBearer(Token);
     }
-    public static class InstanceExtension
+    private bool certificateValidation(object sender,
+                                       System.Security.Cryptography.X509Certificates.X509Certificate certificate,
+                                       System.Security.Cryptography.X509Certificates.X509Chain chain,
+                                       System.Net.Security.SslPolicyErrors policy)
     {
-        public static TSource Get<TSource>(this Instance instance)
-            where TSource : SourceBase
+        if (IgnoreCertificatErrors) return true;
+
+        return policy == System.Net.Security.SslPolicyErrors.None;
+    }
+    public void ConfigureInternalClient(Action<ClientInfo> action)
+    {
+        action(client);
+    }
+
+    public async Task<bool> CheckRunningAsync()
+    {
+        try
         {
-            return (TSource)Activator.CreateInstance(typeof(TSource), instance.client);
+            await IsRunningAsync();
+            return true;
         }
+        catch { return false; }
+    }
+
+    public async Task IsRunningAsync()
+    {
+        var info = await client.GetAsync<string>("/api/");
+        if (!info.IsSuccessStatusCode) throw new Exceptions.ClientException($"Request to `/api/` Failed with code {info.StatusCode}", info);
+    }
+    public async Task<Models.CoreStateModel> GetCoreState()
+    {
+        var info = await client.GetAsync<Models.CoreStateModel>("/api/core/state");
+        if (!info.IsSuccessStatusCode) throw new Exceptions.ClientException($"Request to `/api/core/state` Failed with code {info.StatusCode}", info);
+
+        return info.Data;
+    }
+    public async Task<string> ErrorLogsAsync()
+    {
+        var info = await client.GetAsync<string>("/api/error_log");
+        if (!info.IsSuccessStatusCode) throw new Exceptions.ClientException($"Request to `/api/error_log` Failed with code {info.StatusCode}", info);
+
+        return info.Data;
+    }
+}
+public static class InstanceExtension
+{
+    public static TSource Get<TSource>(this Instance instance)
+        where TSource : SourceBase
+    {
+        return (TSource)Activator.CreateInstance(typeof(TSource), instance.client);
     }
 }
--- Simple.HAApi/SourceBase.cs
+++ Simple.HAApi/SourceBase.cs
@@ -1,40 +1,39 @@
-﻿using Simple.API;
+﻿namespace Simple.HAApi;
+
+using Simple.API;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Simple.HAApi
+public abstract class SourceBase
 {
-    public abstract class SourceBase
+    protected ClientInfo client { get; private set; }
+    protected SourceBase(ClientInfo info)
     {
-        protected ClientInfo client { get; private set; }
-        protected SourceBase(ClientInfo info)
-        {
-            client = info;
-        }
-
-        protected async Task<T> GetAsync<T>(string path)
-        {
-            var info = await client.GetAsync<T>(path);
-            if (!info.IsSuccessStatusCode) throw new Exceptions.ClientException($"Request to `{path}` Failed with code {info.StatusCode}", info);
-
-            return info.Data;
-        }
-        protected async Task<T> PostAsync<T>(string path, object data)
-        {
-            var info = await client.PostAsync<T>(path, data);
-            if (!info.IsSuccessStatusCode) throw new Exceptions.ClientException($"Request to `{path}` Failed with code {info.StatusCode}", info);
-
-            return info.Data;
-        }
-        protected async Task<T> PostJsonAsync<T>(string path, string json)
-        {
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
-            var info = await client.PostAsync<T>(path, content);
-            if (!info.IsSuccessStatusCode) throw new Exceptions.ClientException($"Request to `{path}` Failed with code {info.StatusCode}", info);
-
-            return info.Data;
-        }
-
+        client = info;
     }
+
+    protected async Task<T> GetAsync<T>(string path)
+    {
+        var info = await client.GetAsync<T>(path);
+        if (!info.IsSuccessStatusCode) throw new Exceptions.ClientException($"Request to `{path}` Failed with code {info.StatusCode}", info);
+
+        return info.Data;
+    }
+    protected async Task<T> PostAsync<T>(string path, object data)
+    {
+        var info = await client.PostAsync<T>(path, data);
+        if (!info.IsSuccessStatusCode) throw new Exceptions.ClientException($"Request to `{path}` Failed with code {info.StatusCode}", info);
+
+        return info.Data;
+    }
+    protected async Task<T> PostJsonAsync<T>(string path, string json)
+    {
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+        var info = await client.PostAsync<T>(path, content);
+        if (!info.IsSuccessStatusCode) throw new Exceptions.ClientException($"Request to `{path}` Failed with code {info.StatusCode}", info);
+
+        return info.Data;
+    }
+
 }
