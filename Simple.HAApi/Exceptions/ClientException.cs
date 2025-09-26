9bbe9944c0836fbaf4a84f3dfa0fcaaa830969c9
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-09-26 12:11:51 -0300

    Refactor Exceptions namespace

--- Simple.HAApi/Exceptions/ClientException.cs
+++ Simple.HAApi/Exceptions/ClientException.cs
@@ -1,16 +1,15 @@
-﻿using Simple.API;
+﻿namespace Simple.HAApi.Exceptions;
+
+using Simple.API;
 using System;
 
-namespace Simple.HAApi.Exceptions
+public class ClientException : Exception
 {
-    public class ClientException : Exception
-    {
-        public Response Info { get; }
+    public Response Info { get; }
 
-        public ClientException(string message, Response info)
-            : base(message)
-        {
-            Info = info;
-        }
+    public ClientException(string message, Response info)
+        : base(message)
+    {
+        Info = info;
     }
 }
